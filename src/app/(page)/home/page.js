a7528523e8245aa141424a3b85b8a84ca3c92854
sakib <sbsakibsarkar8@gmail.com>
2025-05-02 11:45:27 +0600

    home edit

--- src/app/(page)/home/page.js
+++ src/app/(page)/home/page.js
@@ -17,7 +17,9 @@ function Home() {
           <h3 className=' font-bold text-[28px]'>And I'm a </h3>
           <h2 className='home_title'><HomeTyping/></h2>
         </div>
-        <p className='home_paragraph'>I am Shakib. front end developer. I have been doing web development since 2023.The reason for coming to programming is, actually I like coding a lot, and thinking about the future, I think programming is the best option in the future.I don't know if my coding is better than everyone else's. But I try my best to write good coding so that everyone will fall in love with my coding.I am now in Dubai. I am working in hotel and also doing programming and learning new things.Being a Bengali, working alone was very difficult for me. Now I want a front end development job.Pray for me, that I can do something good…</p>
+        <p className='home_paragraph'>I love building creative and user-friendly web applications.
+         I am always ready to take on new challenges and learn constantly.
+         If you want to turn your idea into reality, let's work together!…</p>
          
          <MediaAndButton/>
         
